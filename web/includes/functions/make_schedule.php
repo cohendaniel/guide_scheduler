4cc78e92fc10f869f866182aa5737b8bacbc9616
Daniel Cohen <dcohen@bowdoin.edu>
2015-07-25 15:56:35 -0400

    changed paths for online deployment

--- web/includes/functions/make_schedule.php
+++ web/includes/functions/make_schedule.php
@@ -1,16 +1,19 @@
 <?php
-require('C:\Users\Dan\Desktop\Projects\guide_scheduler\web\includes\database\DB.php');
-
-if ($data = mysql_query("SELECT * FROM guides")) {
-	$path = 'C:/Users/Dan/Desktop/Projects/guide_scheduler/output/outputguides.csv';
+require_once('./includes/database/DB.php');
+echo "about to mysqli query";
+if ($data = mysqli_query($connection, "SELECT * FROM guides")) {
+	echo "mysqli queried";
+	$path = '../output/outputguides.csv';
 	$output = fopen("$path", 'w');
-	while ($row = mysql_fetch_assoc($data)) {
-		//echo $row["Name"];
-		//echo $row["Major"];
+	echo "opened path";
+	while ($row = mysqli_fetch_assoc($data)) {
+		echo $row["Name"];
+		echo $row["Major"];
 		fputcsv($output, $row);
 	}
 	fclose($output);
-	$return = shell_exec("C:\Users\Dan\Desktop\Projects\guide_scheduler\Debug\guide_scheduler.exe $path");
+	echo "about to execute program";
+	$return = shell_exec("..\Debug\guide_scheduler.exe $path");
 	
 	echo $return;
 	return true;
